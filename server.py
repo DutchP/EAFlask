337e0251145329e04cc06f9dc088c7b677f7d5f7
DutchP <dutch.p.hardy@gmail.com>
2020-12-21 00:43:01 +0100

    first commit of py files

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)  # creating the app
+
+# index route
+
+
+@app.route("/")
+def index():
+    return render_template('index.html', Title='Home')
+
+
+# starting the application
+if __name__ == '__main__':
+    app.run(debug=True, port=80)
+
+# We will change this during production and create
+# a WGSI server to run it in a docker
