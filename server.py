f1ead717b038257b17a9df19f336e32b63395a64
DutchP <dutch.p.hardy@gmail.com>
2020-12-21 17:11:18 +0100

    layout aangepast , js en css files aangebracht

--- server.py
+++ server.py
@@ -3,12 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)  # creating the app
 
 # index route
-
-
 @app.route("/")
 def index():
     return render_template('index.html', Title='Home')
-
 
 # starting the application
 if __name__ == '__main__':
@@ -16,3 +13,5 @@ if __name__ == '__main__':
 
 # We will change this during production and create
 # a WGSI server to run it in a docker
+# This server is intended to be the api to the EvertRobles website 
+# to provide it of the necessary data 
