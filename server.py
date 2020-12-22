237ebd432b475e45acd0e1388f3ac5dca75fbd50
giovanniherdigein <giovanni.herdigein@gmail.com>
2020-12-22 23:50:53 +0000

    server aangepast route toegeveogd

--- server.py
+++ server.py
@@ -7,6 +7,10 @@ app = Flask(__name__)  # creating the app
 def index():
     return render_template('index.html', Title='Home')
 
+@app.route("/EZine")
+def e_magezine():
+    return render_template("")
+
 # starting the application
 if __name__ == '__main__':
     app.run(debug=True, port=80)
