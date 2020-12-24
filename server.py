b7a54559d032015adf88e8a98c1d0fbb7b38851f
DutchP <dutch.p.hardy@gmail.com>
2020-12-24 15:55:03 +0100

    bestanden toevoegen voor verandering

--- server.py
+++ server.py
@@ -2,20 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)  # creating the app
 
+
 # index route
 @app.route("/")
 def index():
     return render_template('index.html', Title='Home')
 
+
+@app.route('/<string:cat>')
+def get_cat(cat):
+    print(cat)
+    return render_template('index.html', Title='cat')
+
+
 @app.route("/EZine")
 def e_magezine():
     return render_template("")
+
 
 # starting the application
 if __name__ == '__main__':
     app.run(debug=True, port=80)
 
 # We will change this during production and create
-# a WGSI server to run it in a docker
+# a WSGI server to run it in a docker
 # This server is intended to be the api to the EvertRobles website 
-# to provide it of the necessary data 
+# to provide it of the necessary data
