0fc499bd18874a5320c2dc5666ac699ab164dce4
DutchP <dutch.p.hardy@gmail.com>
2020-12-28 09:33:29 +0100

    nu geupload

--- models.py
+++ models.py
@@ -10,18 +10,18 @@ class Article(db.Model):
     body = db.Column(db.Text)
 
     def __init__(self,subject='',body=''):
-        this.subject = subject
-        this.body = body
+        self.subject = subject
+        self.body = body
 
     def __str__(self):
-        return this.subject
+        return self.subject
 
 class Image(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(40),nullable=False,unique=True)
     image_url = db.Column(db.String(220),nullable=False, unique=True)
     date_created= db.Column(db.DateTime(),nullable=False)
-    description = db.Column(db.Text()) 
+    description = db.Column(db.Text) 
 
     def __str__(self):
-        return f"{this.name}, {this.image_url}"
+        return f"{self.name}, {self.image_url}"
